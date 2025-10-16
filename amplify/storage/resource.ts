d8327fe2cb0d359c11df12bd4a5d5f9d372387c6
Kazuyasu Tsurumaki <tsurumaki.kazuyasu@canon-mj.co.jp>
2025-10-16 16:46:38 +0900

    fix: フォルダ名の追加
     Changes to be committed:
    	modified:   amplify/storage/resource.ts

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -4,17 +4,29 @@ export const storage = defineStorage({
   name: 'myStorageBucket',
   isDefault: true,
    access: (allow) => ({
-    'public/*': [
+    'bpoai_prod1/*': [
         allow.guest.to(['read', 'write']),
         allow.authenticated.to(['read', 'write', 'delete']),
     ],
-    'admin/*': [
-        allow.groups(['admin']).to(['read', 'write', 'delete']),
-        allow.authenticated.to(['read'])
+    'bpoai_prod2/*': [
+        allow.guest.to(['read', 'write']),
+        allow.authenticated.to(['read', 'write', 'delete']),
     ],
-    'private/{entity_id}/*': [
-        allow.entity('identity').to(['read', 'write', 'delete'])
+    'bpoai_test1/*': [
+        allow.guest.to(['read', 'write']),
+        allow.authenticated.to(['read', 'write', 'delete']),
+    ],
+    'bpoai_test2/*': [
+        allow.guest.to(['read', 'write']),
+        allow.authenticated.to(['read', 'write', 'delete']),
     ]
+    // 'admin/*': [
+    //     allow.groups(['admin']).to(['read', 'write', 'delete']),
+    //     allow.authenticated.to(['read'])
+    // ],
+    // 'private/{entity_id}/*': [
+    //     allow.entity('identity').to(['read', 'write', 'delete'])
+    // ]
    })
 });
 
